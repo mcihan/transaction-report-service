4043f8649e9dd917cbffce15153967dba3879755
cihan.dogan <cihan.dogan@asseco-see.com.tr>
2023-11-11 21:12:30 +0000

    init

---
+++ src/main/java/com/cihan/paymentreportservice/PaymentReportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cihan.paymentreportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentReportServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentReportServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cihan/paymentreportservice/PaymentReportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cihan.paymentreportservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentReportServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
